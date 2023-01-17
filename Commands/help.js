018890e88fd96f03544b172bc5bd5003d25ee1fc
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-01-17 20:47:55 +0700

    Update help command

--- Commands/help.js
+++ Commands/help.js
@@ -11,8 +11,8 @@ module.exports = {
         const CatHelp = new EmbedBuilder()
             .setColor(16580861)
             .setTitle(`คำสั่งข่วยเหลือ - Mao`)
-            .setDescription("นี้คือคำสั่งทั้งหมดของหนู !")
-            .addFields(
+            .setDescription("นี้คือคำสั่งทั้งหมดของหนู !\n.....\nกำลังปรับปรุ่งระบบ help")
+            /*.addFields(
                 { name: 'แมว หรือ <@1060182470630330529>', value: 'เป็นการคุยกับบอท', inline: true },
                 { name: 'หิว หรือ กิน', value: 'เป็นการใช้ระบบสุ่มอาหาร', inline: true },
                 { name: 'มีม หรือ meme', value: 'เป็นการให้บอทหามีมให้', inline: true },
@@ -20,7 +20,7 @@ module.exports = {
                 { name: 'mao!setting', value: 'เป็นการดูตั้งค่าของบอท', inline: true },
                 { name: 'mao!help', value: 'เป็นการดูคำสั่งทั้งหมดของบอท', inline: true },
                 { name: 'mao!rps', value: 'เป็นการเล่นเกมเป่ายิงชุบกับบอท', inline: true },
-            )
+            )*/
             .setThumbnail("https://cdn.discordapp.com/attachments/988037995531759658/1064139171121336330/OIP-removebg-preview.png")
             .setTimestamp()
             .setFooter({ text: 'Bot help - V.0.1 BETA'});
