46296d05b18cd74634ea0ae8b673810bbb0c12b9
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-02-05 20:30:13 +0700

    Update Error info

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ client.on('messageCreate', async message => {
         }
         
         prompt = await translate(prompt, {to: 'en'}).catch(error => {
-            message.reply("ERR =>  `"+error+"`")
+            message.reply("เกิดข้อผิดพลาด =>  `"+error+"`  ลองพิมพ์อีกครั้งดูสิ !")
             return
         });
         prompt += `${client.user.username}:`
@@ -110,17 +110,27 @@ client.on('messageCreate', async message => {
 
         //console.log("response", response.data.choices[0].text)
         const Finishm = await translate(response.data.choices[0].text, {to: 'th'}).catch(error => {
-            message.reply("ERR =>  `"+error+"`")
+            message.reply("เกิดข้อผิดพลาด =>  `"+error+"`  ลองพิมพ์อีกครั้งดูสิ !")
             return
         });
         //await message.channel.send(Finishm)
 
-        const ResponseAnswer = new EmbedBuilder()
+        if (Finishm == "undefined") {
+            const ResponseError = new EmbedBuilder()
+            .setColor(16711680)
+            .setAuthor({ name: ` :  มีบ้างอย่างผิดผลาด !` , iconURL: 'https://cdn.discordapp.com/attachments/1061529756203499571/1071290286166265856/00006-3271186202-Anime_girl_cat.png'})
+            .setTimestamp()
+
+            message.reply({ embeds : [ResponseError] });
+        } else {
+            const ResponseAnswer = new EmbedBuilder()
             .setColor(15401215)
             .setAuthor({ name: ` :  ${Finishm}` , iconURL: 'https://cdn.discordapp.com/attachments/1061529756203499571/1071290286166265856/00006-3271186202-Anime_girl_cat.png'})
             .setTimestamp()
 
-        message.reply({ embeds : [ResponseAnswer] });
+            message.reply({ embeds : [ResponseAnswer] });
+        }
+        
     }
 });
 console.log("[CMD] : Loaded ChatBot")
