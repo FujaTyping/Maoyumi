5135cef973c5bca43e8249b2e5f7cdcac815e012
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-03-03 15:25:18 +0700

    Fix ping warn

--- Commands/Utils/ping.js
+++ Commands/Utils/ping.js
@@ -18,7 +18,7 @@ module.exports = {
         const Wanmove = new EmbedBuilder()
         .setColor(16777215)
         .setAuthor({ name: "คำสั่งนี้ถูกย้ายเป็น Slash command เรียบร้อยแล้ว !" , iconURL: 'https://cdn.discordapp.com/attachments/1071401485239332864/1073205416328183908/00000-4163793642-Anime_girl_cat_purple_smile.png'})
-        .setDescription('ลองใช้คำสั่ง `/ping` แทนดูสิ')
+        .setDescription('ลองใช้คำสั่ง `/bot ping` แทนดูสิ')
         .setFooter({ text: '⚠ หากไม่สารถใช้ Slash command ได้ กรุณาเชิญบอทใหม่ !'})
         .setTimestamp()
 
