fb2bd77963acee9abfc1a9bff62793245567244e
Siraphop Sukchu <soraphopsukchu@gmail.com>
2023-03-13 19:59:32 +0700

    Update stop msg

--- Commands/Music/stop.js
+++ Commands/Music/stop.js
@@ -8,6 +8,7 @@ module.exports = {
     },
     async run (client,message,args) {
         const MusicAuthorprofile = client.config.defultauthorprofile
+        const Botname = client.config.botname
         if (!message.member.voice.channel) {
             const NotinVC = new EmbedBuilder()
               .setColor(16711680)
@@ -19,7 +20,7 @@ module.exports = {
             const ConLeave = new EmbedBuilder()
                 .setColor(14024959)
                 .setAuthor({ name: `ออกจากห้องแล้วคะ !` , iconURL: `${MusicAuthorprofile}`})
-                .setDescription('.....\nขอบคุณที่ใช้บริการ Maoyumi น้า~~~\nสามารถสนับสนุนบอทได้ที่ :  <:patreon:1084812403268669450>  [Patreon](https://www.patreon.com/FujaTyping/membership)')
+                .setDescription(`ขอบคุณที่ใช้บริการ ${Botname} น้า~~~\nสามารถสนับสนุนบอทได้ที่ :  <:patreon:1084812403268669450>  [Patreon](https://www.patreon.com/FujaTyping/membership)`)
                 .setTimestamp()
 
             message.channel.send({ embeds : [ConLeave] })
